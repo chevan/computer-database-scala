64cf94cab91ca36dd0ac5d653c7cf6a565d117bf
chevan <c.chen@campus.tu-berlin.de>
2015-05-03 22:56:52 +0200

    sql

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -8,7 +8,7 @@ insert into company (id,name) values (  2,'Thinking Machines');
 
 insert into computer (id,name,introduced,discontinued,company_id) values (  1,'search',null,null,1);
 insert into computer (id,name,introduced,discontinued,company_id) values (  2,'analyse',null,null,2);
-insert into computer (id,name,introduced,discontinued,company_id) values (  3,'cut',null,null,2);
+insert into computer (id,name,introduced,discontinued,company_id) values (  3,'cut',null,2);
 
 
 
